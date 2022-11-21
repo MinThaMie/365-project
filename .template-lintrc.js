093d4c68f57c1e630ad928d872ea28cb82720bf9
Anne-Greeth van Herwijnen <a.g.vanherwijnen@outlook.com>
2022-11-21 21:33:31 +0100

    Photos 49 - 61

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,4 +2,8 @@
 
 module.exports = {
   extends: 'recommended',
+  rules: {
+    'no-curly-component-invocation': { allow: ['days-left'] },
+    'no-implicit-this': { allow: ['days-left'] },
+  },
 };
--- tests/integration/helpers/days-left-test.js
+++ tests/integration/helpers/days-left-test.js
@@ -2,16 +2,23 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'anne-greeth/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import MockDate from 'mockdate';
 
 module('Integration | Helper | daysLeft', function (hooks) {
   setupRenderingTest(hooks);
+  // TODO: Replace this with your real tests.
+  test('it renders 364', async function (assert) {
+    MockDate.set(new Date('9/20/2022'));
+    await render(hbs`{{days-left}}`);
+    assert.dom(this.element).hasText('364');
+    MockDate.reset();
+  });
 
   // TODO: Replace this with your real tests.
-  test('it renders', async function (assert) {
-    this.set('inputValue', '1234');
-
-    await render(hbs`{{days-left this.inputValue}}`);
-
-    assert.dom(this.element).hasText('1234');
+  test('it renders 1', async function (assert) {
+    MockDate.set(new Date('9/18/2023'));
+    await render(hbs`{{days-left}}`);
+    assert.dom(this.element).hasText('1');
+    MockDate.reset();
   });
 });
